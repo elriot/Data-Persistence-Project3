92ebf607d451c69115c542719b90fbe7476297df
SKim <45351992+elriot@users.noreply.github.com>
2025-03-12 12:58:36 -0700

    InputText bug fixed
    data type should be TMP_InputField instead of InputText

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -1,23 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using TMPro;
 
+
+[DefaultExecutionOrder(1000)]
 public class MenuUIHandler : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	public TMP_InputField userName;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+	void Start()
+	{
+		// userName = GameObject.Find("NameInput").GetComponent<InputField>();
+	}
 	public void StartGame()
 	{
-		Debug.Log("start");
+		Debug.Log("start : " + userName.text);
 		SceneManager.LoadScene(1);
+	}
+
+
+	public void ExitGame()
+	{
+#if UNITY_EDITOR
+		EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
 	}
 }
